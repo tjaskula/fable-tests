ae6d30eabcabaf0b5c47a9790ebd228d024ae253
Tomasz Jaskula <thomasv1000@hotmail.fr>
2017-06-17 00:10:07 +0200

    add restore.sh

---
+++ restore.sh
@@ -0,0 +1,6 @@
+yarn install
+dotnet restore Fable.proj
+#dotnet restore Fable.Samples.sln
+# Uncomment the previous line and remove all the following line when 
+# this issue https://github.com/dotnet/cli/issues/6723 is fixed
+dotnet restore ReactVehSearch/ReactVehSearch.fsproj
