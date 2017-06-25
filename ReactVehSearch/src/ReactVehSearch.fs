62052b9cf0a25934b27f9d8b599e0352c5824126
Tomasz Jaskula <thomasv1000@hotmail.fr>
2017-06-25 02:00:13 +0200

    Add data selection

--- ReactVehSearch/src/ReactVehSearch.fs
+++ ReactVehSearch/src/ReactVehSearch.fs
@@ -4,6 +4,7 @@ open System
 open Fable.Core
 open Fable.Core.JsInterop
 open Fable.Import
+open Microsoft.FSharp.Reflection
 
 // JS utility for conditionally joining classNames together
 let classNames(classes: obj): string =
@@ -23,17 +24,69 @@ module Util =
     let save key (data: 'T) =
         Browser.localStorage.setItem(key, JS.JSON.stringify data)
 
+// this simulates the database
+type VehiculeItemDataRecord = { id: int; label: string; parentId: int; category: string }
+let datas = [ { id = 1; label = "Renault"; parentId = 0; category = "Make" }
+            ; { id = 2; label = "Volkswagen"; parentId = 0; category = "Make" }
+            ; { id = 3; label = "Mercedes"; parentId = 0; category = "Make" }
+            ; { id = 4; label = "BMW"; parentId = 0; category = "Make" }
+            ; { id = 5; label = "Opel"; parentId = 0; category = "Make" }
+            ; { id = 6; label = "Clio"; parentId = 1; category = "Model" }
+            ; { id = 7; label = "Megane"; parentId = 1; category = "Model" }
+            ; { id = 8; label = "Talisman"; parentId = 1; category = "Model" }
+            ; { id = 9; label = "Passat"; parentId = 2; category = "Model" }
+            ; { id = 10; label = "Tuareg"; parentId = 2; category = "Model" }
+            ; { id = 11; label = "C"; parentId = 3; category = "Model" }
+            ; { id = 12; label = "X6"; parentId = 4; category = "Model" }
+            ; { id = 13; label = "Série 5"; parentId = 4; category = "Model" }
+            ; { id = 14; label = "Corsa"; parentId = 5; category = "Model" }
+            ; { id = 15; label = "Insignia"; parentId = 5; category = "Model" }
+            ; { id = 16; label = "Essence"; parentId = 9; category = "Fuel" }
+            ; { id = 17; label = "Diesel"; parentId = 9; category = "Fuel" }
+            ; { id = 18; label = "Manuelle"; parentId = 16; category = "Transmission" }
+            ; { id = 19; label = "Automatique"; parentId = 16; category = "Transmission" }
+            ; { id = 20; label = "Sémi-automatique"; parentId = 16; category = "Transmission" }
+            ; { id = 21; label = "Manuelle"; parentId = 17; category = "Transmission" }
+            ; { id = 22; label = "Automatique"; parentId = 17; category = "Transmission" }
+            ; { id = 23; label = "Bérline"; parentId = 22; category = "Body" }
+            ; { id = 24; label = "Break"; parentId = 22; category = "Body" }
+            ; { id = 25; label = "14"; parentId = 24; category = "FiscalPower" }
+            ; { id = 26; label = "7"; parentId = 24; category = "FiscalPower" }
+            ]
+
 // Model
 type VehicleItem = { id: int; label: string }
+
 type SearchStep =
 | Make
 | Model
 | Fuel
-| Transmissio
+| Transmission
 | Body
 | FiscalPower
+| Result
 
 type Search = { id: Guid; lastChoice: SearchStep option; selectedChoices: VehicleItem list; showResults: bool }
+
+let getNextSearchStep currentSearchStep =
+    match currentSearchStep with
+    | Make -> Model
+    | Model -> Fuel
+    | Fuel -> Transmission
+    | Transmission -> Body
+    | Body -> FiscalPower
+    | FiscalPower -> Result
+    | Result -> Result
+
+[<PassGenericsAttribute>]
+let getData category selectedId =
+    let toString (x:'a) = 
+        match FSharpValue.GetUnionFields(x, typeof<'a>) with
+        | case, _ -> case.Name
+    datas
+    |> List.filter (fun e -> e.category = toString category && e.parentId = selectedId)
+    |> List.map (fun e -> { id = e.id; label = e.label})
+    |> List.sortBy (fun e -> e.label)
 
 type SearchModel(key) =
     member val key = key
@@ -48,7 +101,7 @@ type SearchModel(key) =
         this.onChanges |> Seq.iter (fun cb -> cb())
 
     member this.select(selectedItem) =
-        Browser.console.log(selectedItem)
+        // Browser.console.log(selectedItem)
         this.search <- { this.search with selectedChoices =  selectedItem :: this.search.selectedChoices }
         this.inform()
 
@@ -68,7 +121,7 @@ type SearchItem(props) =
     do base.setInitState({ isSelected = false })
 
     member this.render () =
-        Browser.console.log("In Render of SearchItem")
+        // Browser.console.log("In Render of SearchItem")
         R.a [
             ClassName "list-group-item"
             !!("data-id", this.props.selectionItem.id |> string)
@@ -88,7 +141,7 @@ type SearchItemList(props) =
     do base.setInitState({ isSelected = false })
 
     member this.render () =
-        Browser.console.log("In Render of SearchItemList")
+        // Browser.console.log("In Render of SearchItemList")
         let searchChoices = this.props.searchChoices
         let brandItems =
             searchChoices
@@ -100,33 +153,43 @@ type SearchItemList(props) =
         R.div [ClassName "row"] [
             R.div [ClassName "col-md-6 col-md-offset-3"] [
                 R.h1 [] [R.str "Vehicle Search"]
-                R.h2 [] [R.str "Select Brand"]
-                R.div [ClassName "list-group"] brandItems 
+                R.h4 [] [R.str "Select Brand"]
+                R.div [ClassName "row"] [
+                    R.div [ClassName "col-md-4"] [
+                        R.div [ClassName "list-group"] brandItems
+                    ]
+                ]
             ]
         ] |> Some
 
 // component
 type [<Pojo>] SearchItemListContainerProps =
     { searchStep: SearchStep
-    ; selectedId: int option 
+    ; selectedId: int 
     ; onSelect: VehicleItem->unit }
 
 type [<Pojo>] SearchItemListContainerState =
-    { searching: Guid option
-    ; searchChoices: VehicleItem list }
+    { searchChoices: VehicleItem list }
 
 type SearchItemListContainer(props) =
     inherit React.Component<SearchItemListContainerProps, SearchItemListContainerState>(props)
-    do base.setInitState({ searching = System.Guid.NewGuid() |> Some; searchChoices = [] })
+    do base.setInitState({ searchChoices = [] })
 
     member this.componentDidMount () =
-        Browser.console.log("In componentDidMount of SearchItemListContainer")
+        //Browser.console.log("In componentDidMount of SearchItemListContainer")
         // this could be an ajax call (this is called only once)
-        let searchChoices = [{ id = 1; label = "Mercedes"}; { id = 2; label = "BMW" }]
+        let searchChoices = getData this.props.searchStep this.props.selectedId
+        this.setState({this.state with searchChoices = searchChoices})
+
+    member this.componentWillReceiveProps(nextProps) =
+        //Browser.console.log("In componentWillReceiveProps of SearchItemListContainer")
+        Browser.console.log(JS.JSON.stringify nextProps)
+        // this could be an ajax call (this is called only once)
+        let searchChoices = getData nextProps.searchStep nextProps.selectedId
         this.setState({this.state with searchChoices = searchChoices})
 
     member this.render () =
-        Browser.console.log("In Render of SearchItemListContainer")
+        //Browser.console.log("In Render of SearchItemListContainer")
         R.com<SearchItemList,_,_> { searchChoices = this.state.searchChoices; onSelect = this.props.onSelect } []
 
 // Vehicle Search view app
@@ -135,18 +198,19 @@ type [<Pojo>] VehicleSearchAppProps =
 
 type [<Pojo>] VehicleSearchAppState =
     { currentStep: SearchStep
-    ; selectedId: int option
+    ; selectedId: int
     ; searching: Guid option }
 
 type VehicleSearchApp(props) =
     inherit React.Component<VehicleSearchAppProps, VehicleSearchAppState>(props)
-    do base.setInitState({ currentStep = Make; selectedId = None; searching = System.Guid.NewGuid() |> Some })
+    do base.setInitState({ currentStep = Make; selectedId = 0; searching = System.Guid.NewGuid() |> Some })
 
-    member this.select (searchStep) =
-        this.props.model.select(searchStep)
+    member this.select (selectedItem) =
+        this.props.model.select(selectedItem)
+        this.setState({ this.state with currentStep = getNextSearchStep this.state.currentStep; selectedId = selectedItem.id })
 
     member this.render () =
-        Browser.console.log("In Render of App")
+        //Browser.console.log("In Render of App")
         R.com<SearchItemListContainer,_,_> 
             { selectedId = this.state.selectedId
             ; searchStep = this.state.currentStep 
